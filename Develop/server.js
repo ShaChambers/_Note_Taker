e6f606d964e73ab72b94622869121302f5c363de
Shaka Suzy <shaheedchambers@gmail.com>
2023-01-09 22:47:01 -0500

    added to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -13,3 +13,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use("/api", api);
 //routes go below!
+//NOTES-GET-ROUTE
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+//INDEX-GET-ROUTE
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+//make sure app is listening
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
